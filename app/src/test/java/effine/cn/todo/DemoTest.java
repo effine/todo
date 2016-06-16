3940d147a0fdb8593099ad24f805b2cb768320a2
effine <iballader@gmail.com>
2016-06-16 23:53:47 +0800

    update

---
+++ app/src/test/java/effine/cn/todo/DemoTest.java
@@ -0,0 +1,58 @@
+package effine.cn.todo;
+
+/**
+ * Created by effine on 6/6/16.
+ */
+public class DemoTest {
+    private int id;
+    private String name;
+    private String age;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+
+        if (o == null || getClass() != o.getClass()) return false;
+
+        DemoTest demoTest = (DemoTest) o;
+
+        return new org.apache.commons.lang3.builder.EqualsBuilder()
+                .append(id, demoTest.id)
+                .append(name, demoTest.name)
+                .append(age, demoTest.age)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode() {
+        return new org.apache.commons.lang3.builder.HashCodeBuilder(17, 37)
+                .append(id)
+                .append(name)
+                .append(age)
+                .toHashCode();
+    }
+}
