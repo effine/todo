439b01a29191c36a0bf26a0a29c47d7e7fb40b0a
effine <iballader@gmail.com>
2016-06-18 23:47:59 +0800

    update

--- app/src/test/java/effine/cn/todo/DemoTest.java
+++ app/src/test/java/effine/cn/todo/DemoTest.java
@@ -35,8 +35,6 @@ public class DemoTest {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        
-
         if (o == null || getClass() != o.getClass()) return false;
 
         DemoTest demoTest = (DemoTest) o;
